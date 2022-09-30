a695fd9565ae18cd599958e6180de03c66030f88
Stella-Obiahu <stellapinky85@gmail.com>
2022-10-01 01:02:32 +1300

    update

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-function callMe(){
-    location.replace("https://socialmedialight.netlify.app/")
-}
-function callMe2(){
-    location.replace("http://127.0.0.1:5500/index.html")
-}
+// function callMe(){
+//     location.replace("https://socialmedialight.netlify.app/")
+// }
+// function callMe2(){
+//     location.replace("http://127.0.0.1:5500/index.html")
+// }
+function callMe() {
+    debugger
+    var element = document.getElementById("name1");
+    var element2 = document.getElementById("body");
+    var forwhite = document.getElementById("whitew");
+    var inputs = document.getElementsByTagName("input");
+    for(var i = 0; i < inputs.length; i++) {
+        if(inputs[i].checked == false) {
+            debugger
+            element.classList.remove("main_1");
+            element2.classList.remove("danny1");
+            forwhite.classList.remove("white");
+            forwhite.classList.add("black");
+            // inputs[i].checked = true; 
+        }
+        else {
+            if(inputs[i].checked == true) {
+                debugger
+                element.classList.add("main_1");
+                element2.classList.add("danny1");
+                forwhite.classList.add("white");
+                // inputs[i].checked = false; 
+             }   
+        }
+    } 
+  }
