c764c726ab01ab3c0f5c313310c13573611d250a
Stella-Obiahu <stellapinky85@gmail.com>
2022-10-01 22:44:15 +1300

    update

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 // function callMe2(){
 //     location.replace("http://127.0.0.1:5500/index.html")
 // }
+
+
+
 var mere = document.querySelectorAll(".card");
 for (var i = 0; i < mere.length; i++) {
     // Add the class margin to the individual elements.
@@ -18,17 +21,19 @@ function callMe() {
     var ov = document.getElementById("overview");
     var cards = document.querySelectorAll(".card");
     var whiite = document.querySelectorAll(".whiite");
+    var wey = document.querySelectorAll(".wey");
+    var pink = document.getElementsByClassName("pink")
     for(var i = 0; i < inputs.length; i++) {
         if(inputs[i].checked == false) {
             debugger
             element.classList.remove("main_1");
+            element.classList.add("main_11");
             element2.classList.remove("danny1");
             forwhite.classList.remove("white");
             forwhite.classList.add("black");
             ov.classList.remove("white");
-            ov.classList.add("black");
-            // cards.classList.remove("card1");
-            // cards.classList.add("card2");
+            ov.classList.add("wey");
+            pink.classList.add("toggle1");
             for (var i = 0; i < cards.length; i++) {
                 // Add the class margin to the individual elements.
                 cards[i].classList.remove('card1');
@@ -38,6 +43,11 @@ function callMe() {
                 // Add the class margin to the individual elements.
                 whiite[i].classList.remove('white');
                 whiite[i].classList.add('black');
+              }
+              for (var i = 0; i < wey.length; i++) {
+                // Add the class margin to the individual elements.
+                wey[i].classList.remove('white');
+                wey[i].classList.add('wey');
               }
             // inputs[i].checked = true; 
         }
@@ -56,6 +66,10 @@ function callMe() {
                   for (var i = 0; i < whiite.length; i++) {
                     // Add the class margin to the individual elements.
                     whiite[i].classList.add('white');
+                  }
+                  for (var i = 0; i < wey.length; i++) {
+                    // Add the class margin to the individual elements.
+                    wey[i].classList.add('wey');
                   }
                 // inputs[i].checked = false; 
              }   
